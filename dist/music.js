01edfd434f1b7137e850f04375fb55a3d68fedad
Huangliuxin <245079931@qq.com>
2018-08-10 22:32:32 +0800

    Site updated: 2018-08-10 22:32:30

--- dist/music.js
+++ dist/music.js
@@ -19,7 +19,7 @@ const ap = new APlayer({
         name: '������',
         artist: '������',
         url: 'http://up.mcyt.net/?down/46644.mp3',
-        cover: 'http://oeff2vktt.bkt.clouddn.com/image/96.jpg',
+        cover: 'http://oeff2vktt.bkt.clouddn.com/image/8.jpg',
       }
     ]
 });
